59f9ae0a73739a550812a011abf5725f997abf0a
Yucheng Ding <ydin0092@student.monash.edu>
2026-07-11 16:50:20 +0800

    Update Lab3 activity 5

--- yding-library/src/components/Form.vue
+++ yding-library/src/components/Form.vue
@@ -1,25 +1,133 @@
 <template>
-    <div class="form">
-    <h1>User Information Form / Credentials</h1>
-        <form>
-            <label for="username">Username:</label><br>
-            <input type="text" id="username" name="username"><br>
-            
-            <label for="password">Password:</label><br>
-            <input type="password" id="password" name="password"><br>
-            
-            <label for="isAustralian">Australian Resident?</label><br>
-            <input type="checkbox" id="isAustralian" name="isAustralian"><br>
-            
-            <label for="reason">Reason For Joining:</label><br>
-            <textarea id="reason" name="reason" rows="3"></textarea><br>
-            
-            <label for="gender">Gender</label><br>
-            <select id="gender">
-                <option value="female">Female</option>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <h1 class="text-center">User Information Form</h1>
+
+        <form @submit.prevent="submitForm">
+          <div class="row mb-3">
+            <div class="col-md-6">
+              <label for="username" class="form-label">
+                Username
+              </label>
+
+              <input
+                id="username"
+                v-model="formData.username"
+                type="text"
+                class="form-control"
+              />
+            </div>
+
+            <div class="col-md-6">
+              <label for="password" class="form-label">
+                Password
+              </label>
+
+              <input
+                id="password"
+                v-model="formData.password"
+                type="password"
+                class="form-control"
+              />
+            </div>
+          </div>
+
+          <div class="row mb-3">
+            <div class="col-md-6">
+              <div class="form-check">
+                <input
+                  id="isAustralian"
+                  v-model="formData.isAustralian"
+                  type="checkbox"
+                  class="form-check-input"
+                />
+
+                <label
+                  for="isAustralian"
+                  class="form-check-label"
+                >
+                  Australian Resident?
+                </label>
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <label for="gender" class="form-label">
+                Gender
+              </label>
+
+              <select
+                id="gender"
+                v-model="formData.gender"
+                class="form-select"
+              >
                 <option value="male">Male</option>
+                <option value="female">Female</option>
                 <option value="other">Other</option>
-            </select>
+              </select>
+            </div>
+          </div>
+
+          <div class="mb-3">
+            <label for="reason" class="form-label">
+              Reason for joining
+            </label>
+
+            <textarea
+              id="reason"
+              v-model="formData.reason"
+              class="form-control"
+              rows="3"
+            ></textarea>
+          </div>
+
+          <div class="text-center">
+            <button
+              type="submit"
+              class="btn btn-primary me-2"
+            >
+              Submit
+            </button>
+
+            <button
+              type="button"
+              class="btn btn-secondary"
+              @click="clearForm"
+            >
+              Clear
+            </button>
+          </div>
         </form>
+      </div>
     </div>
+  </div>
 </template>
+
+<script setup>
+import { reactive } from 'vue'
+
+const formData = reactive({
+  username: '',
+  password: '',
+  isAustralian: false,
+  gender: 'male',
+  reason: ''
+})
+
+const submitForm = () => {
+  console.log('Form submitted:', formData)
+}
+
+const clearForm = () => {
+  formData.username = ''
+  formData.password = ''
+  formData.isAustralian = false
+  formData.gender = 'male'
+  formData.reason = ''
+}
+</script>
+
+<style scoped>
+</style>
+
